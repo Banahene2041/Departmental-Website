03b5ad44a38bfc71a2993d1f105997412ed12beb
Banahene2041 <0542342728c@gmail.com>
2024-09-03 03:43:54 -1200

    second commit

--- src/screens/Chat.jsx
+++ src/screens/Chat.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Chat = () => {
   return (
-    <div>Type your message</div>
+    <>
+    <Header/>
+      <div>Type your message</div>
+    </>
   )
 }
 
