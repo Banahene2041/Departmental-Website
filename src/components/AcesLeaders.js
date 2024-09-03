59c400c591a5810b59feaa4fd8ae179e797cc590
Banahene2041 <0542342728c@gmail.com>
2024-09-02 23:57:18 -1200

    second commit

--- src/components/AcesLeaders.js
+++ src/components/AcesLeaders.js
@@ -3,18 +3,19 @@ import { acesLeaders } from "../data"
 
 const AcesLeaders = () => {
   return (
-    <article className='grid grid-cols-1 gap-2 md:gap-5 lg:gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-3 lg:mt-10'>
+    <article className='grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
       {acesLeaders.map((item, index) => {
         const { position, image, name, year } = item
         return (
-          <div
-            key={index}
-            className='p-4 aces-leaders border rounded-lg text-lg lg:text-3xl text-[#080872] md:text-2xl'
-          >
-            <img src={image} alt={name} className='w-full h-auto rounded-md' />
+          <div key={index} className='.aces-leaders p-4 border rounded-lg'>
+            <img
+              src={image}
+              alt={name}
+              className='leaders w-full md:h-[55vh] lg:h-[60vh] object-cover rounded-md'
+            />
             <h4 className='mt-2 font-semibold'>{position}</h4>
-            <p className='text-xl my-1 md:my-2 lg:my-3'>{year}</p>
-            <p className='text-xl'>{name}</p>
+            <p className='text-sm text-gray-600'>{year}</p>
+            <p className='text-lg'>{name}</p>
           </div>
         )
       })}
